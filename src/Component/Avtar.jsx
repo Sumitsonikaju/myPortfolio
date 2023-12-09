6b4a2ade7afeec1a25d1c7b027b8861e000ec159
Sumitsonikaju <sumitsoni79059@gmail.com>
2023-12-09 17:17:41 +0530

    image change

--- src/Component/Avtar.jsx
+++ src/Component/Avtar.jsx
@@ -4,7 +4,7 @@ const Avtar = () => {
   return (
     <>
       <div className="flex justify-center px-[100px] items-center min-h-[15vh] py-8">
-        <img src="https://www.cartoonbucket.com/wp-content/uploads/2015/05/Pokemon-With-Ash-Image-600x376.png" alt="my Avtar" />
+        <img src="src\Component\Image\sumit_image_passport.jpg" alt="my Avtar" />
       </div>
     </>
   );
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <div className="flex justify-between px-[100px] items-center min-h-[15vh]">
-        <img src="src\Component\Image\sumit_image_passport.jpg" alt="logo" />
+        <img src="https://mattfarley.ca/img/favicon-32.p" alt="logo" />
         <div>
           <button className="px-4 py-2 border-0 rounded-full">Projects</button>
           <button className="px-4 py-2 border-2 ml-2 rounded-full border-[rgb(110,7,243)]"> Resume </button>
